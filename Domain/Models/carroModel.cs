c665d6c1bd8e780b5c3a6781827855df80afe7f6
erlalopezc <lopezerla@gmail.com>
2023-07-06 21:28:02 -0600

    Prueba

---
+++ Domain/Models/carroModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class carroModel
+    {
+        public int Anio { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+    }
+}

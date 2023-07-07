822ee1e7629a21a7658ddb46bf6141d5f2347741
usuario <usuario@DESKTOP-2LD9BP0>
2023-07-06 21:00:32 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CarroController.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPrueba.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarroController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post([FromBody] carroModel Carro)
+        {
+            if (Carro.Modelo == "X3") {
+                Carro.Placa = "BMW";
+            }
+            return Ok(Carro);
+            
+
+
+        }
+    }
+}
